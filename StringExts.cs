e354ce3d2cafda99486862aa08fc026ccacaf47a
hellun205 <hellun515@gmail.com>
2023-01-19 03:22:31 +0900

    convert without watermark

---
+++ StringExts.cs
@@ -0,0 +1,23 @@
+﻿using System.Drawing.Imaging;
+
+namespace WebpConverter;
+
+public static class StringExts
+{
+  public static ImageFormat ToImageFormat(this string ext)
+  {
+    return ext.ToLower() switch
+    {
+      "bmp" => ImageFormat.Bmp,
+      "emf" => ImageFormat.Emf,
+      "exif" => ImageFormat.Exif,
+      "gif" => ImageFormat.Gif,
+      "ico" => ImageFormat.Icon,
+      "jpg" => ImageFormat.Jpeg,
+      "jpeg" => ImageFormat.Jpeg,
+      "png" => ImageFormat.Png,
+      "tiff" => ImageFormat.Tiff,
+      "wmf" => ImageFormat.Wmf,
+    };
+  }
+}
